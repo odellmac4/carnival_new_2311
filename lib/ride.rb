dc20b756514646a8338cd418b23d9c93b503ae8a
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-09 09:18:31 -0800

    Feat: create a rider log with each visitor and the amount of times they ride the ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,6 +1,11 @@
 class Ride
 
-    attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
+    attr_reader :name, 
+                :min_height, 
+                :admission_fee, 
+                :excitement, 
+                :total_revenue,
+                :rider_log
 
     def initialize(info)
         @name = info[:name]
@@ -8,5 +13,12 @@ class Ride
         @admission_fee = info[:admission_fee]
         @excitement = info[:excitement]
         @total_revenue = 0
+        @rider_log = Hash.new { |h_obj, k| h_obj[k] = 0 }
+    end
+
+    def board_rider(visitor)
+        if visitor.preferences.include?(@excitement)
+            @rider_log[visitor] += 1
+        end
     end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 RSpec.describe Ride do
     before(:each) do
         @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        @visitor1 = Visitor.new('Bruce', 54, '$10')
+        @visitor2 = Visitor.new('Tucker', 36, '$5')
     end
 
-    describe 'visitor' do
+    describe 'ride' do
       it 'is a ride ' do
         expect(@ride1).to be_an_instance_of (Ride)
       end
@@ -29,6 +31,21 @@ RSpec.describe Ride do
       it 'total revenue defaults to 0' do
         expect(@ride1.total_revenue).to eq (0)
       end
+    end
+
+    describe '#board_rider and #rider_log' do
+        it 'visitor is a rider on the ride and ride logs riders' do
+            @visitor1.add_preference(:gentle)
+            @visitor2.add_preference(:gentle)
+            @ride1.board_rider(@visitor1)
+            @ride1.board_rider(@visitor2)
+            @ride1.board_rider(@visitor1)
+            
+            expect(@ride1.rider_log).to eq({
+                @visitor1 => 2,
+                @visitor2 => 1 
+            })
+        end
     end
 end
   
