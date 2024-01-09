c247559edcd26dfb04af2f65a2e3f38b651bca9f
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-09 08:08:58 -0800

    Feat: initialize and test visitor and its attributes

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 
+RSpec.describe Visitor do
+    before(:each) do
+        @visitor1 = Visitor.new('Bruce', 54, '$10')
+  
+    end
+    describe 'visitor' do
+      it 'is a visitor' do
+        expect(@visitor1).to be_an_instance_of (Visitor)
+      end
+  
+      it 'has a name' do
+        expect(@visitor1.name).to eq ("Bruce")
+      end
+  
+      it 'defaults to having no ship' do
+        expect(@visitor1.height).to eq (54)
+      end
+  
+      it 'defaults to being empty' do
+        expect(@visitor1.spending_money).to eq ('$10')
+      end
+    end
+end
